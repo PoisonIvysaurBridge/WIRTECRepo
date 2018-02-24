e86e16eb936e0f89d747da506b7888cce92bb631
p01s0n.ivy <ivana_lim@dlsu.edu.ph>
2018-02-24 11:05:35 +0800

    added song java model

---
+++ app/src/main/java/com/example/android/wir_tecrepo/Song.java
@@ -0,0 +1,26 @@
+package com.example.android.wir_tecrepo;
+
+public class Song {
+    private long songID;
+    private String songName;
+    private String artist;
+
+    public Song(long songID, String songName, String artist) {
+        this.songID = songID;
+        this.songName = songName;
+        this.artist = artist;
+    }
+
+
+    public long getSongID() {
+        return songID;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+}
